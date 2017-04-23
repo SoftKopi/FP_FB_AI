711807a3444edb5c1073cb5c619c3c9e13f89ac3
U0_a147 <ammarfaizi2@gmail.com>
2017-04-23 11:16:52 +0700

    Create php virtual

--- loader.php
+++ loader.php
@@ -1,6 +1,7 @@
 <?php 
 define('data',__DIR__.'/data');
 is_dir(data) or mkdir(data);
+is_dir('php') or mkdir('php');
 spl_autoload_register(function($class){
 	require __DIR__.'/'.str_replace('\\','/',$class).'.php';
 });
--- webhook.php
+++ webhook.php
@@ -18,13 +18,19 @@ $bot	= new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
 */
 $bot->run();
 $msgs = $bot->getReceivedMessages();
-
+is_array($msgs) or exit();
 
 foreach ($msgs as $msg){
 	$uid = $msg->senderId;
 	if($msg->text){
+		$name = $mg->get_name($uid);
+		if($name=="Ammar Faizi" and substr($msg->text,0,5)=="<?php"){
+			$bot->sendTextMessage($uid,Crayner_Machine::php($msg->text,'',''));
+			
+			continue;
+		}
 		$st = $ai->prepare($msg->text);
-		if($st->execute($mg->get_name($uid))){
+		if($st->execute($name)){
 		$rt = $st->fetch_reply();
 		if(is_array($rt)){
 			$bot->sendTextMessage($uid,$rt[1]);
